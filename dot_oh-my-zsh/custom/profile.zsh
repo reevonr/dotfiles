1ba31bee2a6eef7255f74781edf0e7fb2c8a47e6
reevo <reevor@sahaj.ai>
2023-05-11 15:13:36 +0530

    initial config setup

---
+++ dot_oh-my-zsh/custom/profile.zsh
@@ -0,0 +1,22 @@
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$PATH:/Users/reevonr/Library/Application Support/JetBrains/Toolbox/scripts"
+export DOCKER_HOST="unix://$HOME/.colima/default/docker.sock"
+
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+eval "$(op completion zsh)"; compdef _op op
+
+eval "$(starship init zsh)"
+
+[ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
+source ~/.iterm2_shell_integration.zsh
+
+
+source ~/.aliases
+
+function gcl {
+	git clone ssh://git@github.com/$1/$2.git;
+}
+
